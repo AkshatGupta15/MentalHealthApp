216f44c2e85ed73a1320d8fe15a99292b95dff3c
AkshatIITk <iitkakshat@gmail.com>
2024-06-26 03:42:32 +0530

    Revert "changes in auth to fix bug"
    This reverts commit d7ba27c126acea1a7a69fe9833456ad24b704dd4.

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,48 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:mentalheathstartup/phone_auth.dart';
 import 'package:mentalheathstartup/screens/home_page.dart';
 import 'package:mentalheathstartup/screens/login_page.dart';
 import 'package:mentalheathstartup/theme_notifier.dart';
-import 'package:mentalheathstartup/wrapper.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  bool isAuthenticated = false;
+  String initialRoute = '/login';
+
   runApp(
-     ChangeNotifierProvider(
+    ChangeNotifierProvider(
       create: (_) => ThemeNotifier(), 
-      child: MyApp(),
+      child: MyApp(
+        initialRoute: initialRoute,
+      ),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String initialRoute;
+
+  const MyApp({
+    required this.initialRoute,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final themeNotifier = Provider.of<ThemeNotifier>(context);
 
-    return GetMaterialApp(
-      title: 'Mental Health App',
+    return MaterialApp(
+      title: 'IITK Mail-Client',
       theme: themeNotifier.getTheme(),
-      home: Wrapper(),
+      initialRoute: initialRoute,
+      routes: {
+        '/login': (context) => const PhoneAuth(),
+        '/home': (context) => HomePage(),
+      },
       debugShowCheckedModeBanner: false,
     );
   }
